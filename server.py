cb346e5b217cde39ccb88e74589ccada651e7dad
PIKASSUU <1055088943@qq.com>
2026-04-20 23:58:37 +0800

    init

--- server.py
+++ server.py
@@ -1,2 +1,18 @@
 class TupleSpaceServer:
-    
+    def __init__(self, port: int):
+        self.host = '0.0.0.0'
+        self.port = port
+        self.tuple_space: Dict[str, str] = {}
+        self.lock = threading.Lock()
+
+        # Statistics fields
+        self.total_clients = 0
+        self.total_operations = 0
+        self.total_reads = 0
+        self.total_gets = 0
+        self.total_puts = 0
+        self.total_errors = 0
+
+        # Start the statistics thread
+        self.stats_thread = threading.Thread(target=self._print_stats_periodically, daemon=True)
+        self.stats_thread.start()
